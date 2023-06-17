8a567a35e3036f25581cdd005661c96d187b1f5a
fermidst <38422620+fermidst@users.noreply.github.com>
2023-06-17 03:44:45 +0300

    Test

--- src/components/buttons/LinkButton.tsx
+++ src/components/buttons/LinkButton.tsx
@@ -1,7 +1,7 @@
 import { type FC } from 'react';
 
 const LinkButton: FC<{ path?: string; name?: string }> = ({ path, name }) => (
-  <a className="btn btn-primary m-2" href={path}>
+  <a className="btn btn-primary m-2" href={`/web-system-lab1-1${path}`}>
     {name}
   </a>
 );
